13e7616944d22f51c397ee4af7089b41451d1664
eljura111 <juracigiralt1@gmail.com>
2021-10-27 12:37:37 +0200

    popup

---
+++ js/jspopup.js
@@ -0,0 +1,27 @@
+let Names = []
+
+//function impresionnombres(){
+//   document.getElementById("impresionarray").innerHTML = Names;
+//}
+
+
+function pushnames(){
+    Names.push(document.getElementById("inputnombres").value);
+}
+
+
+//let fps = 30;
+
+//setInterval(function(){
+//    impresionnombres()
+//}, 1000/fps)
+
+function mostrarnombres(){
+    let localizacion = document.getElementById("impresionarray");
+    let todolist = ""
+    Names.forEach(elemento => {
+            todolist += `<li>${elemento}</li>`
+    });
+    localizacion.innerHTML = todolist;
+}
+
